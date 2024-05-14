e89126f3978c74a2e6c3be4d1fe75c51d82c7674
leisure <2429120006@qq.com>
2024-05-14 09:47:41 +0800

    fix: 优化使用SSR

--- server/routes/article/detail/[id].ts
+++
@@ -1,6 +0,0 @@
-export default defineEventHandler(async (event) => {
-  const id = getRouterParam(event, 'id')
-  const config = useRuntimeConfig()
-  const { data } = await event.$fetch(`${config.public.BASE_URL}article/detail/${id}`)
-  return data
-})
--- server/routes/system/website.ts
+++
@@ -1,5 +0,0 @@
-export default defineEventHandler(async (event) => {
-  const config = useRuntimeConfig()
-  const { data } = await event.$fetch(`${config.public.BASE_URL}system/website?_t=${new Date().getTime()}`)
-  return data
-})
